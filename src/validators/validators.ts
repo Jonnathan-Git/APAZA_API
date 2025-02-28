04e9b37fb1a70c64a6786aa6db4c7b9c0f0d1dbb
DiegoERS <diegoerios10@gmail.com>
2025-02-28 01:26:44 -0600

    correcion de error de validacion de imagen opcional

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -2,6 +2,7 @@ import {FileTypeValidator, MaxFileSizeValidator, ParseFilePipe } from "@nestjs/c
 
 export function imageValidator() {
   return new ParseFilePipe({
+    fileIsRequired: false, // Permite que el archivo sea opcional
     validators: [
       new MaxFileSizeValidator({ maxSize: 50 * 1024 * 1024 }), // Límite de tamaño de 1 KB
       new FileTypeValidator({ fileType: /(jpg|jpeg|png|gif|webp)$/ })
